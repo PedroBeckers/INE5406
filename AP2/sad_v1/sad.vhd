c558a26983e8af74473f64f740747d8ac525b9ec
Pedro Becker <163075551+PedroBeckers@users.noreply.github.com>
2024-10-29 10:59:13 -0300

    Update sad.vhd
    Arrumei saida da sad

--- AP2/sad_v1/sad.vhd
+++ AP2/sad_v1/sad.vhd
@@ -4,25 +4,12 @@ USE ieee.math_real.ALL;
 
 ENTITY sad IS
 	GENERIC (
-		-- obrigatório ---
-		-- defina as operações considerando o número B de bits por amostra
 		B : POSITIVE := 8; -- número de bits por amostra
-		-----------------------------------------------------------------------
-		-- desejado (i.e., não obrigatório) ---
-		-- se você desejar, pode usar os valores abaixo para descrever uma
-		-- entidade que funcione tanto para a SAD v1 quanto para a SAD v3.
 		N : POSITIVE := 64; -- número de amostras por bloco
 		P : POSITIVE := 1 -- número de amostras de cada bloco lidas em paralelo
-		-----------------------------------------------------------------------
+		
 	);
 	PORT (
-		-- ATENÇÃO: modifiquem a largura de bits das entradas e saídas que
-		-- estão marcadas com DEFINIR de acordo com o número de bits B e
-		-- de acordo com o necessário para cada versão da SAD (tentem utilizar
-		-- os valores N e P descritos acima para criar apenas uma descrição
-		-- configurável que funcione tanto para a SAD v1 quanto para a SAD v3).
-		-- Não modifiquem os nomes das portas, apenas a largura de bits quando
-		-- for necessário.
 		clk : IN STD_LOGIC; -- ck
 		enable : IN STD_LOGIC; -- iniciar
 		reset : IN STD_LOGIC; -- reset
@@ -30,7 +17,7 @@ ENTITY sad IS
 		sample_can : IN STD_LOGIC_VECTOR (B - 1 DOWNTO 0); -- Mem_B[end]
 		read_mem : OUT STD_LOGIC; -- read
 		address : OUT STD_LOGIC_VECTOR (INTEGER(CEIL(log2(real(N)))) - 1 DOWNTO 0); -- end
-		sad_value : OUT STD_LOGIC_VECTOR (B + INTEGER(CEIL(log2(real(N)))) - 1 DOWNTO 0); -- SAD
+		sad_value : OUT STD_LOGIC_VECTOR ((INTEGER(CEIL(REAL(B) / REAL(P))) + INTEGER(CEIL(log2(REAL(N)))) + INTEGER(CEIL(log2(REAL(P))))) - 1 DOWNTO 0); -- Calulo = log2[(N * P) * (2^(B / P) - 1)]  -> basta aplicar log2 no valor maximo possivel
 		done : OUT STD_LOGIC -- pronto
 	);
 END ENTITY; -- sad
