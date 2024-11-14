8eb5e0cf6f7a20e7ac25ded476dfba7ca78e0c25
Pedro Becker <163075551+PedroBeckers@users.noreply.github.com>
2024-11-14 08:52:09 -0300

    Update golden-model.py

--- AP2/sad-v3/golden-model/golden-model.py
+++ AP2/sad-v3/golden-model/golden-model.py
@@ -2,20 +2,21 @@ import random
 
 with open("estimulos.dat", "w") as arq:
 
-    for i in range(50): #50 pares de blocos 8x8
+    for _ in range(16):
+        for __ in range(50): #50 pares de blocos 8x8
 
-        mem_A = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-        mem_B = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-        sad_value = 0
+            mem_A = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+            mem_B = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+            sad_value = 0
 
-        for j in range(16): #loop para leitura inteira da memoria pela sad_v3 
+            for j in range(16): #loop para leitura inteira da memoria pela sad_v3 
+                
+                for k in range(4):
+                    sad_value += abs(mem_A[k] - mem_B[k])
             
-            for k in range(4):
-                sad_value += abs(mem_A[k] - mem_B[k])
-        
-        linha = f"{mem_A[0]:08b}" + f"{mem_A[1]:08b}" + f"{mem_A[2]:08b}" + f"{mem_A[3]:08b}"
-        linha += " " + f"{mem_B[0]:08b}" + f"{mem_B[1]:08b}" + f"{mem_B[2]:08b}" + f"{mem_B[3]:08b}"
-        linha += " " + f"{sad_value:014b}" 
-        
-        arq.write(linha + "\n")
+            linha = f"{mem_A[0]:08b}" + f"{mem_A[1]:08b}" + f"{mem_A[2]:08b}" + f"{mem_A[3]:08b}"
+            linha += " " + f"{mem_B[0]:08b}" + f"{mem_B[1]:08b}" + f"{mem_B[2]:08b}" + f"{mem_B[3]:08b}"
+            linha += " " + f"{sad_value:014b}" 
+            
+            arq.write(linha + "\n")
         
